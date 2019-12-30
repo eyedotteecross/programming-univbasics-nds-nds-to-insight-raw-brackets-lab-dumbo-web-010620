ce03b5bf1d87fe0be3154343d38ac8f1e3aa322e
Anthony Holder <anthonyh202x@gmail.com>
2019-12-30 15:19:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,10 +3,21 @@ require 'directors_database'
 
 def directors_totals(nds)
   pp directors_database
-  result = {}
-  director_index_itr = 0
-  movie_index_itr = 0
-  while index_itr < nds[] do 
-  directors   
-  end 
+    director_index = 0
+  totals = {}
+
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    totals[director_name] = 0
+    movie_index = 0
+
+    while movie_index < nds[director_index][:movies].length do
+      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+
+    director_index += 1
+  end
+
+  totals
 end
