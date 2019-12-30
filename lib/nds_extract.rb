f888e21415247cf3c5e574677678942f4e3b10aa
Anthony Holder <anthonyh202x@gmail.com>
2019-12-30 15:32:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,9 +8,8 @@ def directors_totals(nds)
 
   while director_index < nds.length do
     director_name = nds[director_index][:name]
-    totals[director_name] = 0
+    totals[director_name] = 0 
     movie_index = 0
-
     while movie_index < nds[director_index][:movies].length do
       totals[director_name] += 
       nds[director_index][:movies][movie_index][:worldwide_gross]
